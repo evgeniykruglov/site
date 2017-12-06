08c37727eb1e6f4f72e3c2a6485b8e767eb06ac0
evgeniy.kruglov <evgeniy.kruglov@simbirsoft.com>
2017-12-07 00:11:40 +0400

    Implemented displaying of posts (place and id)

--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -1,24 +1,25 @@
 @extends('layouts.app')
+
 @section('content')
 <div class = "container">
 	<div class="row">
 		<div class="col-md-8 col-md-offset-2">
-			@guest
-			@else
-			<p>
-				<a href="{{url('/post/create}}" class="btn btn-primary">Загрузить фото</a>
-			</p>
-			@endguest
 		<div class="panel panel-default">
 		 	<div class="panel-heading">Лента</div>
 			<div class="panel-body">
-				@forelse($posts as $post)
-				@if(auth()->id() === $post-user_id) 
-					<a href="{{route('post.edit', ['post'=>$post]) }}" class = "btn btn-primary"> Изменить</a>
-<form action="{{route('post.destroy', ['post' => $post] }}" method="POST">
-{{method_field('DELETE')}}
-{{csrf_field()}}
-</form>
-
-					
+				@foreach($posts as $post)
+					@if(auth()->id() == $post->user_id)
+					<div class="panel-heading"> № записи:{{$post->id}} </div>
+						<div class="panel-body"> 
+							Место съемки: {{$post->place}}
+						</div>
+						<hr>						
+					@endif
+				@endforeach
+				
+			</div>
+		</div>
+	</div>
+</div>	
+</div>
 @endsection
